8bd55e4505afaf1156ef5347102cde2a2255e4bd
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2020-07-03 14:24:05 +0900

    ㅇㅇ

---
+++ PetSitter/src/main/java/com/spring/petsitter/MemberUsinglistVO.java
@@ -0,0 +1,84 @@
+package com.spring.petsitter;
+
+
+public class MemberUsinglistVO {
+	private int LIST_NUM;
+	private String LIST_TYPE;
+	private String MEMBER_ID;
+	private String PETSITTER_NAME;
+	private String PETSITTER_NICKNAME;
+	private String PETSITTER_TEL;
+	private String LIST_START_DATE;
+	private String LIST_END_DATE;
+	private int LIST_PRICE;
+	private String LIST_ING; // 현재 이용중, 이용 완료
+	private String LIST_COMPLETE; // 리뷰 남기기, 펫시터와의 소통
+	
+	public int getLIST_NUM() {
+		return LIST_NUM;
+	}
+	public void setLIST_NUM(int lIST_NUM) {
+		LIST_NUM = lIST_NUM;
+	}
+	public String getLIST_TYPE() {
+		return LIST_TYPE;
+	}
+	public void setLIST_TYPE(String lIST_TYPE) {
+		LIST_TYPE = lIST_TYPE;
+	}
+	public String getMEMBER_ID() {
+		return MEMBER_ID;
+	}
+	public void setMEMBER_ID(String mEMBER_ID) {
+		MEMBER_ID = mEMBER_ID;
+	}
+	public String getPETSITTER_NAME() {
+		return PETSITTER_NAME;
+	}
+	public void setPETSITTER_NAME(String pETSITTER_NAME) {
+		PETSITTER_NAME = pETSITTER_NAME;
+	}
+	public String getPETSITTER_NICKNAME() {
+		return PETSITTER_NICKNAME;
+	}
+	public void setPETSITTER_NICKNAME(String pETSITTER_NICKNAME) {
+		PETSITTER_NICKNAME = pETSITTER_NICKNAME;
+	}
+	public String getPETSITTER_TEL() {
+		return PETSITTER_TEL;
+	}
+	public void setPETSITTER_TEL(String pETSITTER_TEL) {
+		PETSITTER_TEL = pETSITTER_TEL;
+	}
+	public String getLIST_START_DATE() {
+		return LIST_START_DATE;
+	}
+	public void setLIST_START_DATE(String lIST_START_DATE) {
+		LIST_START_DATE = lIST_START_DATE;
+	}
+	public String getLIST_END_DATE() {
+		return LIST_END_DATE;
+	}
+	public void setLIST_END_DATE(String lIST_END_DATE) {
+		LIST_END_DATE = lIST_END_DATE;
+	}
+	public int getLIST_PRICE() {
+		return LIST_PRICE;
+	}
+	public void setLIST_PRICE(int lIST_PRICE) {
+		LIST_PRICE = lIST_PRICE;
+	}
+	public String getLIST_ING() {
+		return LIST_ING;
+	}
+	public void setLIST_ING(String lIST_ING) {
+		LIST_ING = lIST_ING;
+	}
+	public String getLIST_COMPLETE() {
+		return LIST_COMPLETE;
+	}
+	public void setLIST_COMPLETE(String lIST_COMPLETE) {
+		LIST_COMPLETE = lIST_COMPLETE;
+	}
+	
+}
---
+++ PetSitter/src/main/java/com/spring/petsitter/UsinglistVO.java
@@ -0,0 +1,74 @@
+package com.spring.petsitter;
+
+import java.util.Date;
+
+/*
+create table USINGLIST(
+    LIST_NUM number(10) primary key,
+    PETSITTER_ID varchar2(30),
+    MEMBER_ID varchar2(30),
+    LIST_PRICE number(8),
+    LIST_START_DATE date,
+    LIST_END_DATE date,
+    LIST_TYPE varchar2(10)
+);
+*/
+public class UsinglistVO {
+	private int LIST_NUM;
+	private String PETSITTER_ID;
+	private String MEMBER_ID;
+	private int LIST_PRICE;
+	private Date LIST_START_DATE;
+	private Date LIST_END_DATE;
+	private int LIST_SCORE;
+	private String LIST_TYPE;
+	
+	public int getLIST_NUM() {
+		return LIST_NUM;
+	}
+	public void setLIST_NUM(int lIST_NUM) {
+		LIST_NUM = lIST_NUM;
+	}
+	public String getPETSITTER_ID() {
+		return PETSITTER_ID;
+	}
+	public void setPETSITTER_ID(String pETSITTER_ID) {
+		PETSITTER_ID = pETSITTER_ID;
+	}
+	public String getMEMBER_ID() {
+		return MEMBER_ID;
+	}
+	public void setMEMBER_ID(String mEMBER_ID) {
+		MEMBER_ID = mEMBER_ID;
+	}
+	public int getLIST_PRICE() {
+		return LIST_PRICE;
+	}
+	public void setLIST_PRICE(int lIST_PRICE) {
+		LIST_PRICE = lIST_PRICE;
+	}
+	public Date getLIST_START_DATE() {
+		return LIST_START_DATE;
+	}
+	public void setLIST_START_DATE(Date lIST_START_DATE) {
+		LIST_START_DATE = lIST_START_DATE;
+	}
+	public Date getLIST_END_DATE() {
+		return LIST_END_DATE;
+	}
+	public void setLIST_END_DATE(Date lIST_END_DATE) {
+		LIST_END_DATE = lIST_END_DATE;
+	}
+	public int getLIST_SCORE() {
+		return LIST_SCORE;
+	}
+	public void setLIST_SCORE(int lIST_SCORE) {
+		LIST_SCORE = lIST_SCORE;
+	}
+	public String getLIST_TYPE() {
+		return LIST_TYPE;
+	}
+	public void setLIST_TYPE(String lIST_TYPE) {
+		LIST_TYPE = lIST_TYPE;
+	}
+}
