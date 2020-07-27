3da2a346641c0062d52523e1e6b836b8f5331998
msh <msh7805@gmail.com>
2020-07-27 11:11:54 +0900

    오류 수정

--- PetSitter/src/main/java/com/spring/petsitter/AdminController.java
+++ PetSitter/src/main/java/com/spring/petsitter/AdminController.java
@@ -33,12 +33,8 @@ public class AdminController {
 
 	@Autowired
 	private UsinglistService usinglistService;
-	
-<<<<<<< HEAD
-	/* 메인 페이지 */
-=======
+
 	/* 관리자 메인 페이지 */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "/admin.me")
 	public String admin_main(Model model,
 			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
@@ -68,11 +64,7 @@ public class AdminController {
 		return "admin/dist/index";
 	}
 	
-<<<<<<< HEAD
-	/* 본문 신고 페이지 */
-=======
 	/* 글 신고 페이지 */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "/admin_reportArticle.me")
 	public String admin_report_article(Model model) {
 		
@@ -125,9 +117,7 @@ public class AdminController {
 		
 		return "admin/dist/member_manage";
 	}
-	
-<<<<<<< HEAD
-=======
+
 	/* 펫시터 신청 목록 페이지 */
 	@RequestMapping(value = "admin_petsitterApply.me")
 	public String admin_petsitter_apply(Model model) {
@@ -139,8 +129,6 @@ public class AdminController {
 		return "admin/dist/apply_petsitter";
 	}
 	
-	
->>>>>>> origin/PGKIM
 	/* 펫시터 관리 페이지 */
 	@RequestMapping(value = "admin_petsitterManage.me")
 	public String admin_petsitter_manage(Model model) {
@@ -165,20 +153,12 @@ public class AdminController {
 		return "admin/dist/petsitter_manage";
 	}
 	
-<<<<<<< HEAD
-	/* 강제 탈퇴 */
-=======
 	/* 회원 탈퇴 */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "/outmember.me")
 	public String member_out(@RequestParam(value="id", required=true) String id) throws Exception {
 		memberService.deleteMember(id);
 		
-<<<<<<< HEAD
-		System.out.println(id + " ���� �Ϸ�");
-=======
 		System.out.println(id + " 회원 탈퇴");
->>>>>>> origin/PGKIM
 		
 		return "redirect:/admin_memberManage.me";
 	}
@@ -187,39 +167,23 @@ public class AdminController {
 	@RequestMapping(value = "suspendedmember.me")
 	public String member_suspended(@RequestParam(value="id", required=true) String id) throws Exception {
 		memberService.suspendedMember(id);
-		
-<<<<<<< HEAD
-		System.out.println(id + " ���� �Ϸ�");
-=======
+
 		System.out.println(id + " 등급 정지");
->>>>>>> origin/PGKIM
 		
 		return "redirect:/admin_memberManage.me";
 	}
 	
-<<<<<<< HEAD
-	/* 회원 복귀 */
-=======
 	/* 회원 복구 */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "rollbackmember.me")
 	public String member_rollback(@RequestParam(value="id", required=true) String id) throws Exception {
 		memberService.rollbackMember(id);
-		
-<<<<<<< HEAD
-		System.out.println(id + " ���� �Ϸ�");
-=======
+
 		System.out.println(id + " 등급 복구");
->>>>>>> origin/PGKIM
 		
 		return "redirect:/admin_memberManage.me";
 	}
-	
-<<<<<<< HEAD
-	/* 등급 변경 */
-=======
+
 	/* 회원 변경 */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "changemember.me")
 	// public void member_change(@RequestParam(value="rank", required=true) String rank, @RequestParam(value="id", required=true) String id) throws Exception {
 	public void member_change(String rank, String id) throws Exception {
@@ -228,23 +192,7 @@ public class AdminController {
 		System.out.println("id : " + id);
 		
 		memberService.changeMember(rank, id);
-		
-<<<<<<< HEAD
-		System.out.println(id + " ���� �Ϸ�");
-	}
-	
-	/* 본문 신고 처리 N -> Y */
-	@RequestMapping(value = "checkArticle.me")
-	public String report_article_check(@RequestParam(value="id", required=true) String id, @RequestParam(value="num", required=true) int num) throws Exception {
-		mreplyService.checkArticle(id, num);
-		
-		System.out.println(id + " ó�� �Ϸ�");
-		
-		return "redirect:/admin_reportArticle.me";
-	}
-	
-	/* 본문 신고 처리 Y -> N */
-=======
+
 		System.out.println(id + " 변경 완료");
 	}
 	
@@ -257,35 +205,21 @@ public class AdminController {
 	}
 	
 	/* 글 처리 Y -> N */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "checkArticle2.me")
 	public String report_article_check2(@RequestParam(value="id", required=true) String id, @RequestParam(value="num", required=true) int num) throws Exception {
 		mreplyService.checkArticle2(id, num);
-		
-<<<<<<< HEAD
-		System.out.println(id + " ó�� �Ϸ�");
-		
-		return "redirect:/admin_reportArticle.me";
-	}
-	
-	/* 리플 신고 처리 N -> Y */
-=======
+
 		return "redirect:/admin_reportArticle.me";
 	}
 	
 	/* 리플 처리 N -> Y */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "checkReply.me")
 	public String report_reply_check(@RequestParam(value="bno", required=true) String bno, @RequestParam(value="rno", required=true) String rno, @RequestParam(value="id", required=true) String id) throws Exception {
 		mreplyService.checkReply(bno, rno, id);
 		return "redirect:/admin_reportReply.me";
 	}
-	
-<<<<<<< HEAD
-	/* 리플 신고 처리 Y -> N */
-=======
+
 	/* 리플 처리 Y -> N */
->>>>>>> origin/PGKIM
 	@RequestMapping(value = "checkReply2.me")
 	public String report_reply_check2(@RequestParam(value="bno", required=true) String bno, @RequestParam(value="rno", required=true) String rno, @RequestParam(value="id", required=true) String id) throws Exception {
 		mreplyService.checkReply2(bno, rno, id);
